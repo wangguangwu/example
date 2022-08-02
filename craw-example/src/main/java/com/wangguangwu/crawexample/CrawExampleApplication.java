b88bd9ba35a25e73ebb8fa4dc1f96a0175b96331
wangguangwu <wangguangwu@pukanghealth.com>
2022-08-02 17:37:23 +0800

    init

---
+++ craw-example/src/main/java/com/wangguangwu/crawexample/CrawExampleApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.crawexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawExampleApplication.class, args);
+    }
+
+}
---
+++ craw-example/src/test/java/com/wangguangwu/crawexample/CrawExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.crawexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
