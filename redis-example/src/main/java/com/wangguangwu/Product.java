043f95b8d5b05d823d068d90ce44d6b5f9fff0a0
wangguangwu <wangguangwu@pukanghealth.com>
2023-01-28 15:49:52 +0800

    add：使用 Redis 缓存对象

---
+++ redis-example/src/main/java/com/wangguangwu/Product.java
@@ -0,0 +1,17 @@
+package com.wangguangwu;
+
+import lombok.Data;
+
+/**
+ * @author wangguangwu
+ */
+@Data
+public class Product {
+
+    private String name;
+
+    private double price;
+
+    private String desc;
+
+}
